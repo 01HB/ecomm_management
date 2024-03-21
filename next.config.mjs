9b5bd5b962747e360546999f17afd5237bd3d16b
01HB <bspaceship3@gmail.com>
2024-03-22 00:30:01 +0600

    initial config setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-};
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      
+    ],
+  },
+}
 
 export default nextConfig;
